2c4b9a1a19bdd4dfcc57535a6292469472cc4514
tstibbs <tstibbs@users.noreply.github.com>
2016-05-09 12:28:48 +0100

    Pull in bing layer from https://github.com/shramov/leaflet-plugins

--- js/app.js
+++ js/app.js
@@ -2,10 +2,15 @@ requirejs.config({
     baseUrl: "js",
     paths: {
 		leaflet: 'http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet',
+		leaflet_bing: 'http://cdnjs.cloudflare.com/ajax/libs/leaflet-plugins/1.6.0/layer/tile/Bing'
     },
     shim: {
         leaflet: {
             exports: 'L'
+        },
+        leaflet_bing: {
+            deps: ['leaflet'],
+            exports: 'L.BingLayer'
         }
     }
 });
--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-define(["leaflet"],
-    function(leaflet) {
+define(["leaflet", "leaflet_bing"],
+    function(leaflet, leaflet_bing) {
         // set up the map
         var map = new leaflet.Map('map');
 
-        // create the tile layer with correct attribution
-        var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-        var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-        var osm = new leaflet.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});		
+        // create bing layer
+        var bingKey = "LfO3DMI9S6GnXD7d0WGs~bq2DRVkmIAzSOFdodzZLvw~Arx8dclDxmZA0Y38tHIJlJfnMbGq5GXeYmrGOUIbS2VLFzRKCK0Yv_bAl6oe-DOc";
+        var bingOsLayer = new leaflet_bing(bingKey, {type: "OrdnanceSurvey", maxNativeZoom: 17});
+        map.addLayer(bingOsLayer);
 
         // start the map in South-East England
         map.setView(new leaflet.LatLng(51.3, 0.7),9);
-        map.addLayer(osm);
     }
 );
