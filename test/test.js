f9d9b3ca3552be85392d7b2cf5d483e6b9c7b2ee
tstibbs <tstibbs@users.noreply.github.com>
2016-05-16 20:09:29 +0100

    Adding reverse conversion method for markers

--- test/test.js
+++ test/test.js
@@ -11,7 +11,13 @@ describe('conversion', function() {
 
 	describe('latLngToGridRef', function () {
 		it('should return actual grid refs', function() {
-			conversion(52.657977, 1.716038).should.equal('TG 51408 13177');
+			conversion.latLngToGridRef(52.657977, 1.716038).should.equal('TG 51408 13177');
+		});
+    });
+
+	describe('osgbToLatLng', function () {
+		it('should work for valid OSBGs', function() {
+			conversion.osgbToLatLng(651409, 313177).should.deepEqual([1.716038442825891, 52.65797660129558]); // array is long, lat
 		});
     });
 });
