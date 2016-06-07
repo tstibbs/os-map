7609625fdfa6a5774a3a35a6055df6390ef010ab
tstibbs <tstibbs@users.noreply.github.com>
2016-06-07 19:52:10 +0100

    More sensible sized marker

--- js/points.js
+++ js/points.js
@@ -1,11 +1,9 @@
 define(["proj4", "leaflet", "os_map", "conversion"],
     function(proj4, leaflet, os_map, conversion) {
 	
-		var icon = L.icon({
+		var icon = leaflet.icon({
 			iconUrl: 'img/trig.png',
-			iconSize: [52, 84],
-			iconAnchor: [22, 83], // point of the icon which will correspond to marker's location
-			popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+			iconAnchor: [10, 40], // point of the icon which will correspond to marker's location
 		});
 	
         return function (eastings, northings) {
