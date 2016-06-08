381f73b1bbd3c195702e30d1586a4826889fa782
tstibbs <tstibbs@users.noreply.github.com>
2016-06-08 12:31:09 +0100

    Removed 'lib' directory in favour of using cdn.rawgit

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ requirejs.config({
 		leaflet: 'http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet',
 		leaflet_bing: 'http://cdnjs.cloudflare.com/ajax/libs/leaflet-plugins/1.6.0/layer/tile/Bing',
 		proj4: 'http://cdnjs.cloudflare.com/ajax/libs/proj4js/2.3.14/proj4',
-		leaflet_mouseposition: '../lib/js/leaflet_mouseposition'
+		leaflet_mouseposition: 'https://cdn.rawgit.com/tstibbs/Leaflet.MousePosition/b628c7be754c134c63117b3feb75e720a1d20673/src/L.Control.MousePosition'
     },
     shim: {
         leaflet: {
