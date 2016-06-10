6aa609bf58b429caa6ad8794fbc9c29bd3e8130e
tstibbs <tstibbs@users.noreply.github.com>
2016-06-10 12:25:21 +0100

    Show crosshairs on click

--- js/screenposition_osgb.js
+++ js/screenposition_osgb.js
@@ -1,13 +1,46 @@
-define(["leaflet_screenposition", "conversion"],
-    function(leaflet_screenposition, conversion) {
+define(["leaflet_screenposition", "conversion", "jquery"],
+    function(leaflet_screenposition, conversion, $) {
         return function() {
-			return leaflet_screenposition({
+			var control = leaflet_screenposition({
 				onMove: true,
-				icon: false,
+				icon: true,
 				latLngFormatter: function (lat, lng) {
 					return conversion.latLngToGridRef(lat, lng);
 				}
 			});
+			
+			function addShowHideHandling() {
+				var $icon = $('div.leaflet-control-mapcentercoord-icon.leaflet-zoom-hide');
+				var isIconVisible = false;
+				var $control = $('div.leaflet-control-mapcentercoord.leaflet-control');
+				
+				function hideIcon() {
+					$icon.css('visibility', 'hidden');
+					isIconVisible = false;
+				}
+				
+				function showIcon() {
+					$icon.css('visibility', 'visible');
+					isIconVisible = true;
+				}
+				
+				$control.click(function() {
+					if (isIconVisible) {
+						hideIcon();
+					} else {
+						showIcon();
+					}
+				});
+				//start with crosshairs hidden
+				hideIcon();
+			}
+			
+			return {
+				addTo: function(map) {
+					control.addTo(map);
+					addShowHideHandling();
+				}
+			};
         }
     }
 );
