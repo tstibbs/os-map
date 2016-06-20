fd3ae6e8036f412669d714e978d564e01822a3fe
tstibbs <tstibbs@users.noreply.github.com>
2016-06-20 15:22:22 +0100

    Adding conversion from gridref strings

--- js/conversion.js
+++ js/conversion.js
@@ -53,8 +53,56 @@ define(["proj4"],
 				return gridRef;
 			},
 
-			osgbToLatLng: function(eastings, northings) {
+			osgbToLngLat: function(eastings, northings) {
 				var out = proj4(osgbProj).inverse([eastings, northings]);//to WSG84
+				return out;
+			},
+			
+			gridRefToOsgb: function(/*String*/ gridref) {
+				gridref = String(gridref).trim();
+
+				// check for fully numeric comma-separated gridref format
+				var match = gridref.match(/^(\d+),\s*(\d+)$/);
+				if (match) return new OsGridRef(match[1], match[2]);
+
+				// validate format
+				match = gridref.match(/^[A-Z]{2}\s*[0-9]+\s*[0-9]+$/i);
+				if (!match) throw new Error('Invalid grid reference');
+
+				// get numeric values of letter references, mapping A->0, B->1, C->2, etc:
+				var l1 = gridref.toUpperCase().charCodeAt(0) - 'A'.charCodeAt(0);
+				var l2 = gridref.toUpperCase().charCodeAt(1) - 'A'.charCodeAt(0);
+				// shuffle down letters after 'I' since 'I' is not used in grid:
+				if (l1 > 7) l1--;
+				if (l2 > 7) l2--;
+
+				// convert grid letters into 100km-square indexes from false origin (grid square SV):
+				var e100km = ((l1-2)%5)*5 + (l2%5);
+				var n100km = (19-Math.floor(l1/5)*5) - Math.floor(l2/5);
+
+				// skip grid letters to get numeric (easting/northing) part of ref
+				var en = gridref.slice(2).trim().split(/\s+/);
+				// if e/n not whitespace separated, split half way
+				if (en.length == 1) en = [ en[0].slice(0, en[0].length/2), en[0].slice(en[0].length/2) ];
+
+				// validation
+				if (e100km<0 || e100km>6 || n100km<0 || n100km>12) throw new Error('Invalid grid reference: gridref='+gridref+', e100km='+e100km+', n100km='+n100km);
+				if (en.length != 2) throw new Error('Invalid grid reference');
+				if (en[0].length != en[1].length) throw new Error('Invalid grid reference');
+
+				// standardise to 10-digit refs (metres)
+				en[0] = (en[0]+'00000').slice(0, 5);
+				en[1] = (en[1]+'00000').slice(0, 5);
+
+				var e = parseInt(e100km + en[0]);
+				var n = parseInt(n100km + en[1]);
+
+				return [e, n];
+			},
+
+			gridRefToLngLat: function(/*String*/ gridref) {
+				var lngLat = this.gridRefToOsgb(gridref);
+				var out = proj4(osgbProj).inverse(lngLat);//to WSG84
 				return out;
 			}
 		};
