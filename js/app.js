871b0a8e26343d5c8be2f4089b8ca303d39fe550
tstibbs <tstibbs@users.noreply.github.com>
2016-06-20 15:33:22 +0100

    Adding marker clustering

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ requirejs.config({
 		leaflet: 'http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet',
 		leaflet_bing: 'http://cdnjs.cloudflare.com/ajax/libs/leaflet-plugins/1.6.0/layer/tile/Bing',
 		proj4: 'http://cdnjs.cloudflare.com/ajax/libs/proj4js/2.3.14/proj4',
+		leaflet_cluster: 'https://cdn.rawgit.com/Leaflet/Leaflet.markercluster/232e93ccbe5b70241913f47a4d1a8ceec8c88c30/dist/leaflet.markercluster-src',
 		leaflet_mouseposition: 'https://cdn.rawgit.com/tstibbs/Leaflet.MousePosition/b628c7be754c134c63117b3feb75e720a1d20673/src/L.Control.MousePosition',
 		leaflet_screenposition: 'https://cdn.rawgit.com/tstibbs/Leaflet.MapCenterCoord/cc990a672930886aaef55b1a66e651bdaaf27353/src/L.Control.MapCenterCoord',
 		jquery: 'https://code.jquery.com/jquery-3.0.0'
@@ -23,6 +24,10 @@ requirejs.config({
         leaflet_screenposition: {
             deps: ['leaflet'],
             exports: 'L.control.mapCenterCoord'
+        },
+        leaflet_cluster: {
+            deps: ['leaflet'],
+            exports: 'L.markerClusterGroup'
         },
         proj4js: {
             exports: 'module.exports'
--- js/points.js
+++ js/points.js
@@ -1,27 +1,43 @@
-define(["proj4", "leaflet", "os_map", "conversion"],
-    function(proj4, leaflet, os_map, conversion) {
+define(["proj4", "leaflet", "os_map", "conversion", "leaflet_cluster"],
+    function(proj4, leaflet, os_map, conversion, leaflet_cluster) {
 	
 		var icon = leaflet.icon({
 			iconUrl: 'img/trig.png',
 			iconAnchor: [10, 40], // point of the icon which will correspond to marker's location
 			popupAnchor: [1, -38] // point from which the popup should open relative to the iconAnchor
 		});
-	
-        return function (eastings, northings, url, name) {
-            var out = conversion.osgbToLngLat(eastings, northings);
-            var lng = out[0];
-            var lat = out[1];
-            var marker = leaflet.marker([lat, lng], {icon: icon});
-			if (name == null) {
-				name = url;
+
+		var markerList = [];
+        return {
+			add: function (eastings, northings, url, name) {
+				var out = conversion.osgbToLngLat(eastings, northings);
+				var lng = lngLat[0];
+				var lat = lngLat[1];
+				var marker = leaflet.marker([lat, lng], {icon: icon});
+				if (name == null) {
+					name = url;
+				}
+				if (url != null) {
+					marker.bindPopup('<a href="' + url + '">' + name + '</a>');
+				} else if (name != null) {
+					marker.bindPopup('<span>' + name + '</span>');
+				}
+				
+				markerList.push(marker);
+				// marker.addTo(os_map.getMap());
+			},
+			finish: function (finished) {
+				var markers = leaflet_cluster({
+					chunkedLoading: true,
+					chunkProgress: function (processed, total, elapsed, layersArray) {
+						if (processed === total) {
+							finished();
+						}
+					}
+				});
+				markers.addLayers(markerList);
+				os_map.getMap().addLayer(markers);
 			}
-			if (url != null) {
-				marker.bindPopup('<a href="' + url + '">' + name + '</a>');
-			} else if (name != null) {
-				marker.bindPopup('<span>' + name + '</span>');
-			}
-			
-			marker.addTo(os_map.getMap());
-        }
+		};
     }
 );
