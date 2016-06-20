663446ee08bb72f33aea630eb1553e51b078c9c9
tstibbs <tstibbs@users.noreply.github.com>
2016-06-20 21:33:22 +0100

    Adding marker clustering

--- js/main.js
+++ js/main.js
@@ -6,15 +6,17 @@ define(["os_map", "points", "params", "jquery"],
 			var allPoints = locationsFromUrl.split(";");
 			for (var i = 0; i < allPoints.length; i++) {
 				var point = allPoints[i].split(',');
-				points(point[0], point[1], point[2], point[3]);
+				points.add(point[0], point[1], point[2], point[3]);
 			}
 		} else {
 			//dummy data as an example
-			points(418678,385093, 'http://trigpointing.uk/trig/6995', 'Winhill Pike');
-			points(422816,385344, 'http://trigpointing.uk/trig/3795', 'High Neb');
-			points(419762,390990, 'http://trigpointing.uk/trig/949', 'Back Tor');
-			points(412927,387809, 'http://trigpointing.uk/trig/3019', 'Edale Moor');
+			points.add(418678, 385093, 'http://trigpointing.uk/trig/6995', 'Winhill Pike');
+			points.add(422816, 385344, 'http://trigpointing.uk/trig/3795', 'High Neb');
+			points.add(419762, 390990, 'http://trigpointing.uk/trig/949', 'Back Tor');
+			points.add(412927, 387809, 'http://trigpointing.uk/trig/3019', 'Edale Moor');
 		}
-		$('div#loading-message-pane').hide();
+		points.finish(function() {
+			$('div#loading-message-pane').hide();
+		});
     }
 );
