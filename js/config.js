75b1ab73df3f7ad476ffff22abfdd9e3769952f3
tstibbs <tstibbs@users.noreply.github.com>
2016-07-14 22:10:13 +0100

    Fix starting position and zoom on mini map

--- js/config.js
+++ js/config.js
@@ -2,7 +2,9 @@ define(["jquery"],
     function($) {
 		var defaults = {
 			cluster: true,
-			dimensional_layering: false
+			dimensional_layering: false,
+			initial_zoom: 13,
+			start_position: [53.374694, -1.711474]//lat, long
 		};
 		
 		var overrides = window.os_map_config;
