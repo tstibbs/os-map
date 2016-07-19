f739629dc8e4a218e6e32ca70e7e4749716d9b3c
tstibbs <tstibbs@users.noreply.github.com>
2016-07-19 12:19:12 +0100

    Updating matrix control plugin

--- tmp/matrixControl.js
+++ tmp/matrixControl.js
@@ -43,8 +43,8 @@ L.Control.MatrixLayers = L.Control.Layers.extend({
 			parentElement.innerHTML = '<p>' + dimensionName + '</p>';
 			parentElement.dimensionName = dimensionName;
 			this._overlaysList.appendChild(parentElement);
-			Object.keys(dimension).forEach(function (dimensionElement) { 
-				this._addMatrixItem(parentElement, dimensionName, dimensionElement);
+			Object.keys(dimension).forEach(function (dimensionValue) { 
+				this._addMatrixItem(parentElement, dimensionName, dimensionValue);
 			}, this);
 		}
 
@@ -58,64 +58,74 @@ L.Control.MatrixLayers = L.Control.Layers.extend({
 		}
 		this._matrixLayers[name] = layer;
 		this.addOverlay(layer, name);
+		this._updateSelectedLayers();//local storage may have affected which checkboxes were ticked, so we need to update which layers are shown
 	},
 	
 	
 	_onMatrixInputClick: function (e) {
 		this._handlingClick = true;
 
-		var labelElement = e.currentTarget.parentElement;
+		var input = e.currentTarget;
+		this._saveSelectionState(input.dimensionName, input.dimensionValue, input.checked);
+		var labelElement = input.parentElement;
 		this._addLoadingIndicator(labelElement, function() {
-			
-			//reset - onInputClick might have got there before us
-			this._handlingClick = true;
-
-			//get the inputs in each div and see which are ticked, to get a list of the selected dimension elements
-			var divs = this._form.getElementsByTagName('div');
-			var checkedDimensions = {};
-			for (var i = 0; i < divs.length; i++) {
-				var div = divs[i];
-				var dimension = div.dimensionName;
-				var inputs = div.getElementsByTagName('input');
-				if (checkedDimensions[dimension] == undefined) {
-					checkedDimensions[dimension] = new Array();
-				}
-				for (var j = 0; j < inputs.length; j++) {
-					var input = inputs[j];
-					if (input.checked) {
-						checkedDimensions[dimension].push(input.dimensionId);
-					}
-				}
-			}
-			//map the group dimension names to an ordering
-			var orderedDimensionElements = new Array();
-			for (var i = 0; i < this.options.dimensionNames.length; i++) {
-				var dimensionName = this.options.dimensionNames[i];
-				var dimensionElements = checkedDimensions[dimensionName];
-				orderedDimensionElements.push(dimensionElements);
-			}
-			//iterate to construct a list of selected layers
-			var selectedLayerNames = {};
-			this._depthFirstIteration(orderedDimensionElements, 0, "", function(path){
-				selectedLayerNames[path] = true;
+			this._updateSelectedLayers(function() {
+				this._removeLoadingIndicator(labelElement);
 			});
-			//now add or remove the layers from the map
-			Object.keys(this._matrixLayers).forEach(function (layerName) {
-				var layer = this._matrixLayers[layerName];
-				if (layerName in selectedLayerNames && !this._map.hasLayer(layer)) {
-					this._map.addLayer(layer);
-
-				} else if (!(layerName in selectedLayerNames) && this._map.hasLayer(layer)) {
-					this._map.removeLayer(layer);
-				}
-			}, this);
-
-			this._removeLoadingIndicator(labelElement);
-
-			this._handlingClick = false;
-
-			this._refocusOnMap();
 		}.bind(this));
+	},
+
+	_updateSelectedLayers: function(callback) {
+		//reset - onInputClick might have got there before us
+		this._handlingClick = true;
+
+		//get the inputs in each div and see which are ticked, to get a list of the selected dimension elements
+		var divs = this._overlaysList.getElementsByTagName('div');
+		var checkedDimensions = {};
+		for (var i = 0; i < divs.length; i++) {
+			var div = divs[i];
+			var dimension = div.dimensionName;
+			var inputs = div.getElementsByTagName('input');
+			if (checkedDimensions[dimension] == undefined) {
+				checkedDimensions[dimension] = new Array();
+			}
+			for (var j = 0; j < inputs.length; j++) {
+				var input = inputs[j];
+				if (input.checked) {
+					checkedDimensions[dimension].push(input.dimensionId);
+				}
+			}
+		}
+		//map the group dimension names to an ordering
+		var orderedDimensionElements = new Array();
+		for (var i = 0; i < this.options.dimensionNames.length; i++) {
+			var dimensionName = this.options.dimensionNames[i];
+			var dimensionElements = checkedDimensions[dimensionName];
+			orderedDimensionElements.push(dimensionElements);
+		}
+		//iterate to construct a list of selected layers
+		var selectedLayerNames = {};
+		this._depthFirstIteration(orderedDimensionElements, 0, "", function(path){
+			selectedLayerNames[path] = true;
+		});
+		//now add or remove the layers from the map
+		Object.keys(this._matrixLayers).forEach(function (layerName) {
+			var layer = this._matrixLayers[layerName];
+			if (layerName in selectedLayerNames && !this._map.hasLayer(layer)) {
+				this._map.addLayer(layer);
+
+			} else if (!(layerName in selectedLayerNames) && this._map.hasLayer(layer)) {
+				this._map.removeLayer(layer);
+			}
+		}, this);
+		
+		if (callback !== undefined) {
+			callback();			
+		}
+
+		this._handlingClick = false;
+
+		this._refocusOnMap();
 	},
 	
 	_depthFirstIteration: function (dimensions, dimensionIndex, parentPath, found) {
@@ -135,21 +145,21 @@ L.Control.MatrixLayers = L.Control.Layers.extend({
 		}
 	},
 
-	_addMatrixItem: function (parentElement, dimensionName, dimensionElement) {
-		var label = document.createElement('label'),
-		    input,
-		    checked = true;//TODO //this._map.hasLayer(obj.layer);
+	_addMatrixItem: function (parentElement, dimensionName, dimensionValue) {
+		var label = document.createElement('label');
 
-		input = document.createElement('input');
+		var input = document.createElement('input');
 		input.type = 'checkbox';
 		input.className = 'leaflet-control-layers-selector';
-		input.defaultChecked = checked;
-		input.dimensionId = dimensionElement;//L.stamp(obj.layer);//TODO//dimensionName + "::" + 
+		input.checked = this._getSelectionState(dimensionName, dimensionValue);
+		input.dimensionId = dimensionValue;//L.stamp(obj.layer);//TODO//dimensionName + "::" + 
+		input.dimensionName = dimensionName;
+		input.dimensionValue = dimensionValue;
 
 		L.DomEvent.on(input, 'click', this._onMatrixInputClick, this);
 
 		var name = document.createElement('span');
-		name.innerHTML = ' ' + dimensionElement;
+		name.innerHTML = ' ' + dimensionValue;
 
 		label.appendChild(input);
 		label.appendChild(name);
@@ -188,7 +198,7 @@ L.Control.MatrixLayers = L.Control.Layers.extend({
 
 	_addLoadingIndicator: function (labelElement, callback) {
 		var img = document.createElement('img');
-		img.src = '../img/loading.gif';
+		img.src = 'loading.gif';
 		img.class = 'loading-image';
 		img.onload = callback;
 		labelElement.appendChild(img);
@@ -197,6 +207,25 @@ L.Control.MatrixLayers = L.Control.Layers.extend({
 	_removeLoadingIndicator: function (labelElement) {
 		var img = labelElement.getElementsByTagName('img')[0];
 		labelElement.removeChild(img);
+	},
+	
+	_saveSelectionState: function(dimensionName, dimensionValue, selected) {
+		if (typeof(Storage) !== "undefined") {
+			localStorage.setItem('matrix_layers_control:' + dimensionName + '/' + dimensionValue, selected.toString());
+		}
+	},
+	
+	_getSelectionState: function(dimensionName, dimensionValue) {
+		if (typeof(Storage) !== "undefined") {
+			var selected = localStorage.getItem('matrix_layers_control:' + dimensionName + '/' + dimensionValue);
+			if (selected == null) {
+				return true;//default to being selected
+			} else {
+				return "true" == selected;//local storage can only store strings
+			}
+		} else {
+			return true;//default to being selected
+		}
 	}
 
 });
