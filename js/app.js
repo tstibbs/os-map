0119462225eee40864babae8366c402f245025b4
tstibbs <tstibbs@users.noreply.github.com>
2016-07-20 12:05:00 +0100

    Removing dependency on window to allow headless build

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
+var urlBase = "";
+if (window != null && window.os_map_base !== undefined) {
+	urlBase = window.os_map_base;
+}
+
 requirejs.config({
-    baseUrl: window.os_map_base + "js",
+    baseUrl: urlBase + "js",
     paths: {
 		leaflet: 'http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet',
 		leaflet_bing: 'http://cdnjs.cloudflare.com/ajax/libs/leaflet-plugins/1.6.0/layer/tile/Bing',
