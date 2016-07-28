c4a00c6bdbc4beb6b134af87f7d12d318c71cc37
tstibbs <tstibbs@users.noreply.github.com>
2016-07-28 12:19:46 +0100

    Adding dev option to load local versions of some files

--- js/app.js
+++ js/app.js
@@ -3,17 +3,24 @@ if (window != null && window.os_map_base !== undefined) {
 	urlBase = window.os_map_base;
 }
 
+var leaflet_path = 'http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet';
+var matrixLayers_path = 'https://cdn.rawgit.com/tstibbs/Leaflet.MatrixLayersControl/ec4781a9ec7bf1b23535df30037805583f08bde2/src/matrixControl';
+if (window.location.search.indexOf("dev=true") !== -1) {
+	leaflet_path = 'http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet-src';
+	matrixLayers_path = urlBase + '../Leaflet.MatrixLayersControl/src/matrixControl';
+}
+
 requirejs.config({
     baseUrl: urlBase + "js",
     paths: {
-		leaflet: 'http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet',
+		leaflet: leaflet_path,
 		leaflet_bing: 'http://cdnjs.cloudflare.com/ajax/libs/leaflet-plugins/1.6.0/layer/tile/Bing',
 		proj4: 'http://cdnjs.cloudflare.com/ajax/libs/proj4js/2.3.14/proj4',
 		leaflet_cluster: 'https://cdn.rawgit.com/Leaflet/Leaflet.markercluster/232e93ccbe5b70241913f47a4d1a8ceec8c88c30/dist/leaflet.markercluster-src',
 		leaflet_mouseposition: 'https://cdn.rawgit.com/tstibbs/Leaflet.MousePosition/b628c7be754c134c63117b3feb75e720a1d20673/src/L.Control.MousePosition',
 		leaflet_screenposition: 'https://cdn.rawgit.com/tstibbs/Leaflet.MapCenterCoord/cc990a672930886aaef55b1a66e651bdaaf27353/src/L.Control.MapCenterCoord',
 		leaflet_subgroup: 'https://cdn.rawgit.com/ghybs/Leaflet.FeatureGroup.SubGroup/e675f5736513ec6f9a8ab391965808b192990e9e/leaflet.featuregroup.subgroup-src',
-        leaflet_matrixlayers: 'https://cdn.rawgit.com/tstibbs/Leaflet.MatrixLayersControl/880e81fb8924d4d85953e28785ff0a29618c0d9d/src/matrixControl',
+        leaflet_matrixlayers: matrixLayers_path,
 		jquery: 'https://code.jquery.com/jquery-3.0.0'
     },
     shim: {
