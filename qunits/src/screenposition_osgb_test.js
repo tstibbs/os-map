41a3fc43803768f1d86ae88608dfaae9c48e89c6
tstibbs <tstibbs@users.noreply.github.com>
2016-08-05 12:40:16 +0100

    Testing screen position

--- qunits/src/screenposition_osgb_test.js
+++ qunits/src/screenposition_osgb_test.js
@@ -1,15 +1,16 @@
-define(["leaflet", "mouseposition_osgb"],
-    function(leaflet, mouseposition_osgb) {
-		QUnit.test("mouseposition_osgb - should display", function(assert) {
+define(["leaflet", "screenposition_osgb"],
+    function(leaflet, screenposition_osgb) {
+		QUnit.test("screenposition_osgb - should display", function(assert) {
 			$('#qunit-fixture').append('<div id="map" style="height: 180px;"></div>');
 			var map = leaflet.map('map');
-			var positionDisplayElement = $('div#map div.leaflet-control-mouseposition');
+			map.setView([51.505, -0.09], 13);
+			var positionDisplayElement = $('div#map div.leaflet-control-mapcentercoord');
 			//check that it hasn't shown up yet, just to validate the rest of our test
 			assert.ok(positionDisplayElement.length === 0);
 			//add the class under test
-			mouseposition_osgb().addTo(map);
+			screenposition_osgb().addTo(map);
 			//now check that the mouse position element is showing up
-			positionDisplayElement = $('div#map div.leaflet-control-mouseposition');
+			positionDisplayElement = $('div#map div.leaflet-control-mapcentercoord');
 			assert.ok(positionDisplayElement.length === 1);
 			assert.ok(positionDisplayElement.is(":visible"));
 		});
