3dabf6d9d9ebf6f9d0681f54c55fe8718668a27d
tstibbs <tstibbs@users.noreply.github.com>
2016-08-07 21:59:14 +0100

    Using karma for all tests part 3: migrating mocha tests to qunit

--- test/conversion_test.js
+++ test/conversion_test.js
@@ -1,35 +1,19 @@
-require('should');
-var requirejs = require("requirejs");
-requirejs.config({
-    baseUrl: 'js',
-    nodeRequire: require
-});
-
-describe('conversion', function() {
-
-	var conversion = requirejs('conversion');
-
-	describe('latLngToGridRef', function () {
-		it('should return actual grid refs', function() {
-			conversion.latLngToGridRef(52.657977, 1.716038).should.equal('TG 51408 13177');
+define(["conversion"],
+    function(conversion) {
+		QUnit.test('conversion - latLngToGridRef - should return actual grid refs', function (assert) {
+			assert.equal(conversion.latLngToGridRef(52.657977, 1.716038), 'TG 51408 13177');
 		});
-    });
 
-	describe('osgbToLngLat', function () {
-		it('should work for valid OSBGs', function() {
-			conversion.osgbToLngLat(651409, 313177).should.deepEqual([1.716038442825891, 52.65797660129558]); // array is long, lat
+		QUnit.test('conversion - osgbToLngLat - should work for valid OSBGs', function (assert) {
+			assert.deepEqual(conversion.osgbToLngLat(651409, 313177), [1.716038442825891, 52.65797660129557]); // array is long, lat
 		});
-    });
 
-	describe('gridRefToOsgb', function () {
-		it('should work for valid grid refs', function() {
-			conversion.gridRefToOsgb('TG 51408 13177').should.deepEqual([651408, 313177]); // array is long, lat
+		QUnit.test('conversion - gridRefToOsgb - should work for valid grid refs', function (assert) {
+			assert.deepEqual(conversion.gridRefToOsgb('TG 51408 13177'), [651408, 313177]); // array is long, lat
 		});
-    });
 
-	describe('gridRefToLngLat', function () {
-		it('should work for valid grid refs', function() {
-			conversion.gridRefToLngLat('TG 51408 13177').should.deepEqual([1.7160236901080013, 52.657977064472796]); // array is long, lat
+		QUnit.test('conversion - gridRefToLngLat - should work for valid grid refs', function (assert) {
+			assert.deepEqual(conversion.gridRefToLngLat('TG 51408 13177'), [1.7160236901080013, 52.657977064472796]); // array is long, lat
 		});
-    });
-});
+	}
+);
