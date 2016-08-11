ab25a5ed79d8c836a8120890fc89a3c1e5f5ddac
tstibbs <tstibbs@users.noreply.github.com>
2016-08-11 22:49:44 +0100

    Using squire to mock window for mobile test so it doesn't upset the browser

--- test/mobile_test.js
+++ test/mobile_test.js
@@ -1,55 +1,49 @@
-define(["mobile"],
-    function(mobile) {
-		QUnit.module( 'mobile', function() {
-			var tempObj = {};
-			try {
-				Object.defineProperty(navigator, "userAgent", { 
-					get: function () { 
-						return tempObj.userAgent;
-					}
-				});
-			} catch (err) {
-				//ignore, we're in phantomjs
-			}
-			function test(userAgent) {
-				tempObj.userAgent = userAgent;
-				navigator.userAgent = userAgent;
-				return mobile.isMobile();
-			}
-			
+define(['Squire', 'mobile'],
+    function(Squire, mobile) {
+		QUnit.module('mobile', function() {		
 			//NOTE: some of these user agents might be munged slightly due to the author's paranoia.
 			
 			QUnit.test('mobile device', function (assert) {
-				navigator = {};
 				// chrome on android
-				assert.ok(test("Mozilla/5.0 (Linux; Android 5.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.0000.00 Mobile Safari/537.36"));
+				runTest(assert, Squire, "Mozilla/5.0 (Linux; Android 5.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.0000.00 Mobile Safari/537.36", true);
 				// firefox on android
-				assert.ok(test("Mozilla/5.0 (Android 5.0.1; Mobile; rv:48.0) Gecko/48.0 Firefox/48.0"));
+				runTest(assert, Squire, "Mozilla/5.0 (Android 5.0.1; Mobile; rv:48.0) Gecko/48.0 Firefox/48.0", true);
 				// android browser
-				assert.ok(test("Mozilla/5.0 (Linux; Android 5.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.0000.00 Mobile Safari/537.36"));
+				runTest(assert, Squire, "Mozilla/5.0 (Linux; Android 5.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.0000.00 Mobile Safari/537.36", true);
 				// safari on ios
-				assert.ok(test("Mozilla/5.0 (iPhone; CPU iPhone OS 6_2_1 like Mac OS X) AppleWebKit/000.0.00 (KHTML, like Gecko) Version/9.0 Mobile/12h64 Safari/600.0"));
+				runTest(assert, Squire, "Mozilla/5.0 (iPhone; CPU iPhone OS 6_2_1 like Mac OS X) AppleWebKit/000.0.00 (KHTML, like Gecko) Version/9.0 Mobile/12h64 Safari/600.0", true);
 			});
 			
 			QUnit.test('mobile pretending to be a desktop', function (assert) {
-				navigator = {};
 				// chrome on android
-				assert.notOk(test("Mozilla/5.0 (X11; Linux x86_64) AppletWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.0000.00 Safari/537.36"));
+				runTest(assert, Squire, "Mozilla/5.0 (X11; Linux x86_64) AppletWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.0000.00 Safari/537.36", false);
 				// firefox on android
-				assert.notOk(test("Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0"));
+				runTest(assert, Squire, "Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0", false);
 				// android browser
-				assert.notOk(test("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.0000.00 Safari/537.36"));
+				runTest(assert, Squire, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.0000.00 Safari/537.36", false);
 			});
 			
 			QUnit.test('desktop', function (assert) {
-				navigator = {};
 				// chrome on a desktop
-				assert.notOk(test("Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.0000.00 Safari/537.36"));
+				runTest(assert, Squire, "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.0000.00 Safari/537.36", false);
 				// firefox on a desktop
-				assert.notOk(test("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0"));
+				runTest(assert, Squire, "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0", false);
 				// IE on a desktop
-				assert.notOk(test("Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"));
+				runTest(assert, Squire, "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko", false);
 			});
 		});
+		
+		function runTest(assert, Squire, userAgent, expected) {
+			var done = assert.async();
+			var injector = new Squire();
+			var mockWindow = {};
+			mockWindow.navigator = {};
+			mockWindow.navigator.userAgent = userAgent;
+			injector.mock('global', mockWindow);
+			injector.require(['mobile'], function(mobile) {
+				assert.equal(expected, mobile.isMobile(), "Expected: " + expected + " for user agent: " + userAgent);
+				done();
+			});
+		}
 	}
 );
