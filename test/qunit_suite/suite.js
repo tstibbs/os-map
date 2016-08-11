722ac5b6523f73e43329468724d36b8d811a324a
tstibbs <tstibbs@users.noreply.github.com>
2016-08-11 20:54:07 +0100

    Fixing bad merge in test suite config

--- test/qunit_suite/suite.js
+++ test/qunit_suite/suite.js
@@ -3,8 +3,8 @@ var testFiles = [
 	"mouseposition_osgb_test.js",
 	"screenposition_osgb_test.js",
 	"points_test.js",
-	"conversion_test.js"
-	"mobile_test.js",
+	"conversion_test.js",
+	"mobile_test.js"
 ];
 
 if (window.__karma__ == undefined) {
