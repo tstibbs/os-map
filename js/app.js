eab7ddfcb0bec28b451bb5e44d26ab4046c3bb6a
tstibbs <tstibbs@users.noreply.github.com>
2016-08-19 20:28:05 +0100

    Fixed incorrect dev path

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ var devPaths = {
 
 if (window.location.search.indexOf("dev=true") !== -1) {
 	paths.leaflet = paths.leaflet + '-src';
-	paths.leaflet_matrixlayers = urlBase + '../Leaflet.MatrixLayersControl/src/matrixControl';
+	paths.leaflet_matrixlayers = '../../Leaflet.MatrixLayersControl/src/matrixControl';
 }
 
 
