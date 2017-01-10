a1a7cf6b9150d39a81e14042fb0898d035b0a0a7
tstibbs <tstibbs@users.noreply.github.com>
2017-01-10 21:48:02 +0000

    Adding friendly names for hill categories

--- js/bundles/hills/config.js
+++ js/bundles/hills/config.js
@@ -3,6 +3,27 @@ define(['leaflet', './points_builder'],
 
 		return {
 			dimensionNames: ['Hills'],
+			dimensionValueLabels: {
+				'Hills': {
+					'5': 'Dewey',
+					'5D': 'Donald Dewey',
+					'5H': 'Highland Five',
+					'C': 'Corbett',
+					'CouT': 'County Top',
+					'D': 'Donald',
+					'G': 'Graham',
+					'Hew': 'Hewitt',
+					'Hu': 'Hump',
+					'M': 'Munro',
+					'MT': 'Munro Top',
+					'Ma': 'Marilyn',
+					'N': 'Nutall',
+					'SIB': 'Islands (SIBs)',
+					'Sim': 'Simm',
+					'TU': 'Tump',
+					'W': 'Wainwright'
+				}
+			},
 			dataToLoad: 'data.json',
 			parser: PointsBuilder
 		};
